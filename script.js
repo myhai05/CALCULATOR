23c1241698be419248f94ed62e1d7d6726874fa8
myhai05 <mmishk0501@gmail.com>
2024-03-09 19:18:07 +0100

    CALCULATOR V.1.1.0

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
     let screen = document.getElementById("display");
-
+    const lastChar = screen.innerHTML.slice(-1);
 
     function plus(){
-        let lastChar = screen.innerHTML.slice(-1);
+       let lastChar = screen.innerHTML.slice(-1);
         if (lastChar === '+' || lastChar === '-' || lastChar === '/' || lastChar === '*' || lastChar === '') { // check if the last character is an operator
                  screen.innerHTML += "";
         } else { screen.innerHTML +="+"; }
@@ -17,7 +17,7 @@
         }
 
     function multiplying(){
-        let lastChar = screen.innerHTML.slice(-1);
+       let lastChar = screen.innerHTML.slice(-1);
         if (lastChar === '+' || lastChar === '-' || lastChar === '/' || lastChar === '*' || lastChar === '') {
                  screen.innerHTML += "";
         } else { screen.innerHTML +="*"; }
@@ -31,18 +31,32 @@
         }
     function zero(){
         let screenValue = screen.innerHTML;
-        screen.innerHTML = (screenValue==="") ? screen.innerHTML="" : screen.innerHTML+=0; //check if the screen is empty
+        let lastChar = screen.innerHTML.slice(-1);
+        if(lastChar==="0"){screenValue==="";} else {screen.innerHTML+=0;} //check if the screen is empty
+        
     }
     function point(){
+        let lastChar = screen.innerHTML.slice(-1); 
         let screenValue = screen.innerHTML;
+        console.log(screenValue);
         screen.innerHTML = (screenValue==="") ? screen.innerHTML="" : screen.innerHTML+='.'; //check if the screen is empty
+        if(lastChar==="."){screen.innerHTML=+"";}// check if the last character is a "." symbol;
     }
-   
-    
-   function equal(){
+    function equal() {
         let screenValue = screen.innerHTML;
-        let result = eval(screenValue);
-        screen.innerHTML = (screenValue==="") ? screen.innerHTML="" : screen.innerHTML=result.toFixed(2); //check if the screen is empty
+        if (screenValue === "") {
+            screen.innerHTML = "";
+            return; // Exit function if display is empty
+        }
+
+        let result;
+    try {
+        result = eval(screenValue);
+    } catch (error) {
+        screen.innerHTML = "ERROR!";
+        return; //exit if the the screenValue can not be evaluate
+    }
+       screen.innerHTML = Number.isInteger(result) ? result : result.toFixed(2);
     }
 
     function reset(){
